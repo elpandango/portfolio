1675dff32433b2619977343e02c56d352dd8a0b7
Pandango <Pandango>
2017-02-12 22:28:56 +0200

    styles and js

--- dist/js/script.js
+++ dist/js/script.js
@@ -31,7 +31,31 @@ $(document).ready(function () {
         }
     });
 
+    //анимированные счетчики
+
+    var show = true;
+    var skillbox = "#skills";
+    $(window).on("scroll load resize", function(){
+
+        if(!show) return false;                   // Отменяем показ анимации, если она уже была выполнена
+
+        var w_top = $(window).scrollTop();        // Количество пикселей на которое была прокручена страница
+        var e_top = $(skillbox).offset().top;     // Расстояние от блока со счетчиками до верха всего документа
+
+        var w_height = $(window).height();        // Высота окна браузера
+        var d_height = $(document).height();      // Высота всего документа
+
+        var e_height = $(skillbox).outerHeight(); // Полная высота блока со счетчиками
+
+        if(w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height){
+            $(".spincrement").spincrement({
+                thousandSeparator: "",
+                duration: 1500
+            });
+
+            show = false;
+        }
+    });
+
 });
-
-
 
